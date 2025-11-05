af32785fde867be9af102c3224629f61cfba3912
Linh Nguyen <76649421+LinhNguyen-97@users.noreply.github.com>
2025-11-05 14:13:06 +0700

    Update dCategory.sql

--- dCategory.sql
+++ dCategory.sql
@@ -1,11 +1,8 @@
-WITH
-  CATEGORY AS (
+SELECT ROW_NUMBER() OVER(ORDER BY `Sub-Category`) AS SubCategoryID
+        ,*
+FROM (
     SELECT DISTINCT
         `Category`
         ,`Sub-Category`
     FROM global-superstore-475905.global_superstore_rawdata.Orders
-    )
-
-SELECT ROW_NUMBER() OVER(ORDER BY `Sub-Category`) AS SubCategoryID
-        ,*
-FROM CATEGORY;
+    );
